704815a6149a45be6c741ae0175344cea532043d
ZJHAO <ZJHAO233@foxmail.com>
2023-04-11 13:25:17 +0800

    HelloWord

---
+++ Day01/src/com/zjhao/HelloWord.java
@@ -0,0 +1,13 @@
+package com.zjhao;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-11
+ * TIME:13:22
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
