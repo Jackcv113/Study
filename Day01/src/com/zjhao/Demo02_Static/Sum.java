7d0c633c47f3f81122789d7e2595f22564d6f55d
ZJHAO <ZJHAO233@foxmail.com>
2023-04-12 10:02:11 +0800

    Static方法

---
+++ Day01/src/com/zjhao/Demo02_Static/Sum.java
@@ -0,0 +1,16 @@
+package com.zjhao.Demo02_Static;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-12
+ * TIME:10:00
+ */
+public class Sum {
+    private Sum() {
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+}
---
+++ Day01/src/com/zjhao/Demo02_Static/Test.java
@@ -0,0 +1,16 @@
+package com.zjhao.Demo02_Static;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-12
+ * TIME:9:59
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        int sum = Sum.sum(a, b);
+        System.out.println(sum);
+    }
+}
