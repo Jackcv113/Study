57bcf356088fef6d524c88ea41f124d95f65b929
ZJHAO <ZJHAO233@foxmail.com>
2023-04-12 09:14:02 +0800

    HelloWord

---
+++ Day01/src/com/zjhao/Demo01_Static/Test.java
@@ -0,0 +1,18 @@
+package com.zjhao.Demo01_Static;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-12
+ * TIME:9:12
+ */
+public class Test {
+    public static void main(String[] args) {
+        User user1 = new User();
+        User user2 = new User();
+        User user3 = new User();
+        User user4 = new User();
+        User user5 = new User();
+        System.out.println(User.count);
+    }
+}
---
+++ Day01/src/com/zjhao/Demo01_Static/User.java
@@ -0,0 +1,15 @@
+package com.zjhao.Demo01_Static;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-12
+ * TIME:9:12
+ */
+public class User {
+    static int count;
+
+    public User() {
+        count++;
+    }
+}
