d812a25d560752ba3016bbefa98838b434795188
ZJHAO <ZJHAO233@foxmail.com>
2023-04-12 17:26:04 +0800

    继承

---
+++ Day01/src/com/zjhao/Demo06_Extends/Student.java
@@ -0,0 +1,56 @@
+package com.zjhao.Demo06_Extends;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-12
+ * TIME:17:23
+ */
+public class Student {
+    String name;
+    int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    /**
+     * 获取
+     * @return name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 设置
+     * @param name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * 获取
+     * @return age
+     */
+    public int getAge() {
+        return age;
+    }
+
+    /**
+     * 设置
+     * @param age
+     */
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return "Student{name = " + name + ", age = " + age + "}";
+    }
+}
---
+++ Day01/src/com/zjhao/Demo06_Extends/Test.java
@@ -0,0 +1,16 @@
+package com.zjhao.Demo06_Extends;
+
+/**
+ * Description:
+ * User:ZJHao
+ * Date:2023-04-12
+ * TIME:17:23
+ */
+public class Test {
+    public static void main(String[] args) {
+        Student stu1 = new Student("张三", 18);
+        System.out.println(stu1);
+        String ss = "abc";
+        System.out.println(ss);
+    }
+}
